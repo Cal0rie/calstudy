ff0d574395b367f9a866ec445e61ebe4b06b2c95
Cal0rie <zmxianyu@gmail.com>
2022-02-25 17:37:48 +0800

    添加学生端底栏和学生端主页

---
+++ components/tab.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class='tab'>
+		<view class='tabbar'>首页</view>
+		<view class='tabbar'>消息</view>
+		<view class='tabbar'>我</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"tab",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+.tab{
+	height: 100rpx;
+	width: 100%;
+	position: fixed;	/* 相对屏幕定位 */
+	bottom :0;
+	left: 0;
+	display: flex;
+	align-content: center;
+}
+.tabbar{
+	float: left;
+	width: 32.583%;
+	color :#000000;
+	background-color: white;
+	text-align: center;
+	height: 100rpx;
+	line-height: 100rpx;
+	font-size: 40rpx;
+	border-style: solid;
+}
+</style>
--- pages/student/index.vue
+++ pages/student/index.vue
@@ -1,37 +1,54 @@
 <template>
 	<view>
-		<view  v-for="(item, index) in list" @click= 'xq(index)'>
-			<view class='tit'>{{index + 1}}  {{blog.blogs[index].title}}</view>
-			<view class='con'>{{blog.blogs[index].content}}</view>
-			</view>
-			<hr>   
+		<view class='cont'>
+		<view class='content'><view>★</view><view>我的考试</view><view>></view></view>
+		<view class='content'><view>★</view><view>日程管理</view><view>></view></view>
+		<view class='content'><view>★</view><view>个人成绩</view><view>></view></view>
+		</view>
+			<tab></tab>
 	    </view>
 </template>
 
 <script>
 import store from '@/store/index.js';//需要引入store
+import tab from '../../components/tab.vue'
 
 var temp=0;
 export default{
+	components:{
+		tab
+	},
 	data() {
 		return {
-			list: store.state.blogs,
-			blog: store.state,
+			// list: store.state.blogs,
+			// blog: store.state,
 		}},
 
     mounted(){
 	},
     methods:{
-		xq(index){
-			uni.navigateTo({
-				url:"/pages/Content",
-				});
-		},
+		// xq(index){
+		// 	uni.navigateTo({
+		// 		url:"/pages/Content",
+		// 		});
+		// },
     }
 }
 </script>
 
 <style>
-	
+	.content{
+		font-size: 50rpx;
+		height: 100rpx;
+		color :#000000;
+		line-height: 100rpx;
+		display: flex;
+		justify-content: space-around;
+		width: 100%;
+	}
+	.cont{
+		/* display: flex;
+		flex-direction: column; */
+	}
 </style>
 
